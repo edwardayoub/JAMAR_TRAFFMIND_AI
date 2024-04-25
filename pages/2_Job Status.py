32e19f2ce36a761b9bba902a96be090b2752d0ab
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-04-25 18:02:57 -0500

    Update 2_Job Status.py

--- pages/2_Job Status.py
+++ pages/2_Job Status.py
@@ -5,11 +5,11 @@ from lib import get_s3_status
 st.title("S3 Bucket Processing Status")
 
 # Manage initial load and refresh with session state
-if 'first_load' not in st.session_state:
-    st.session_state['first_load'] = True
+if 'first_load' not in st.session_state or st.button('Refresh Data'):
+    st.session_state['refresh'] = True
 
 # Auto-refresh on the initial load or when the refresh button is pressed
-if st.session_state['first_load'] or st.button('Refresh Data'):
+if st.session_state['refresh']:
     # Fetch data
     data_df = get_s3_status()
     # Display data
