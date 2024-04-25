42433bb8e0c0e80a296bc0f9a79f0ce3b26e35e6
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-04-25 17:55:12 -0500

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,4 @@
 from .sagemaker_processing import run
-from .aws import download_file, list_files
+from .aws import download_file, list_files, get_s3_status
 
-__all__ = ["run", "download_file", "list_files"]
+__all__ = ["run", "download_file", "list_files", "get_s3_status"]
