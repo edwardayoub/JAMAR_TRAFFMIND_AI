2c96af664e210f691713378807b41ffb345fe8b2
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-04-25 18:04:29 -0500

    Update 2_Job Status.py

--- pages/2_Job Status.py
+++ pages/2_Job Status.py
@@ -4,12 +4,14 @@ from lib import get_s3_status
 # Streamlit page setup
 st.title("S3 Bucket Processing Status")
 
+refresh = st.button('Refresh Data')
+
 # Manage initial load and refresh with session state
-if 'first_load' not in st.session_state or st.button('Refresh Data'):
-    st.session_state['refresh'] = True
+if 'first_load' not in st.session_state:
+    st.session_state['first_load'] = True
 
 # Auto-refresh on the initial load or when the refresh button is pressed
-if st.session_state['refresh']:
+if st.session_state['first_load'] or refresh:
     # Fetch data
     data_df = get_s3_status()
     # Display data
