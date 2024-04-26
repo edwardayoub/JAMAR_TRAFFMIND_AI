22038712a1dc9ed6e27867cf7582a56c4e94d520
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-04-26 09:10:08 -0500

    Update 1_Step 1: Video Upload.py

--- pages/1_Step 1: Video Upload.py
+++ pages/1_Step 1: Video Upload.py
@@ -38,10 +38,7 @@ st.header("TraffMind AI Job Submission")
 
 st.markdown("""
 Welcome to our video traffic analysis portal. Here, you can submit your traffic videos for analysis. Follow the steps below to get started:
-1. **Select a Video**: Choose the video you want to analyze by uploading it.
-2. **Name Your Submission**: Enter a unique name for your submission to easily track your analysis.
-3. **Submit**: Click the submit button to send your video for processing.
-""")
+1. **Select a Video**: Choose the video you want to analyze by uploading it.""")
 
 # File uploader for video selection
 uploaded_video = st.file_uploader("Upload your video", type=['mp4'])
@@ -52,9 +49,11 @@ if uploaded_video is not None:
 else:
     st.sidebar.write("Please upload a video to proceed.")
 
-
+st.markdown("""
+2. **Submit**: Click the submit button to send your video for processing.
+""")
 # Submit button
-if st.sidebar.button("Submit"):
+if st.button("Submit"):
     if uploaded_video is not None:
         st.sidebar.success("Your submission is received!")
         print(uploaded_video.name)
