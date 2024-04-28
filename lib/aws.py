b6b8bd48c42d9ac2ec5f02d7ba8cc55437259178
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-04-28 10:23:39 -0500

    Update aws.py

--- lib/aws.py
+++ lib/aws.py
@@ -59,3 +59,27 @@ def get_s3_status():
     
     return status_df
 
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+def generate_presigned_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object.
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client("s3", region_name=region, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    return response
