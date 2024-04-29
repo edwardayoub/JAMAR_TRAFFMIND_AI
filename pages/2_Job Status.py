800e3c83e4a74b68b261564ccd2729ae1167707f
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-04-28 23:28:11 -0500

    Update 2_Job Status.py

--- pages/2_Job Status.py
+++ pages/2_Job Status.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from lib import get_s3_status
+def show_table_with_links(df):
+    # Convert DataFrame to HTML, replacing text URL with an HTML link
+    df['Download Link'] = df['Download Link'].apply(lambda x: f'<a href="{x}" target="_blank">Download</a>' if x is not None else "")
+    st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
+
 
 # Streamlit page setup
 st.title("S3 Bucket Processing Status")
@@ -15,5 +20,5 @@ if st.session_state['first_load'] or refresh:
     # Fetch data
     data_df = get_s3_status()
     # Display data
-    st.dataframe(data_df)
+    show_table_with_links(data_df)
     st.session_state['first_load'] = False
