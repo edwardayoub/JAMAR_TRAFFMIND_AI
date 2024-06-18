44e048057e5b38a39b05418f542c4ffe0d0bb549
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-17 21:03:22 -0500

    Update 1_Step 1: Video Upload.py

--- pages/1_Step 1: Video Upload.py
+++ pages/1_Step 1: Video Upload.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import logging
-import os
-import streamlit.components.v1 as components
 
 # Initialize logger
 logger = logging.getLogger(__name__)
@@ -12,20 +10,9 @@ st.set_page_config(layout="wide")
 # Header
 st.header("TraffMind AI Job Submission")
 
-# Step 1: Select a video
+# Step 1: Upload Video Using SFTP
 st.markdown("""
-**1. Select a Video**: Upload a video file to start the job submission process. Only MP4 and h264 formats are supported.
-""")
-
-# Iframe for file upload and submission
-components.iframe("https://traffmind-upload-ui.s3.us-east-2.amazonaws.com/index.html", height=125)
-
-# Placeholder to capture the file name from the iframe
-uploaded_video_name = st.empty()
-
-# New Step: Upload Video Using SFTP
-st.markdown("""
-**2. Upload Video Using SFTP**
+**1. Upload Video Using SFTP**
 
 You can now upload your videos using an SFTP client such as Cyberduck. Follow the steps below:
 
@@ -67,7 +54,7 @@ You can now upload your videos using an SFTP client such as Cyberduck. Follow th
 
 # Link to check status
 st.markdown("""
-**3. Draw Vectors**: Before submitting the job, you can draw vectors on the video to track vehicles.
+**2. Draw Vectors**: Before submitting the job, you can draw vectors on the video to track vehicles.
 """)
 
 st.page_link(
