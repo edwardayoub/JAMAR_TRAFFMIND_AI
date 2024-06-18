e0b787e236ef3bf663c98c46d094a35152ec32e7
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2024-06-17 19:45:05 -0500

    Update 1_Step 3: Traffic Tracker and Classifier.py

--- pages/1_Step 3: Traffic Tracker and Classifier.py
+++ pages/1_Step 3: Traffic Tracker and Classifier.py
@@ -1,5 +1,7 @@
+import os
 import streamlit as st
 from lib import get_s3_status
+
 region = 'us-east-2'
 access_key = os.getenv("AWS_ACCESS_KEY_ID")
 secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
