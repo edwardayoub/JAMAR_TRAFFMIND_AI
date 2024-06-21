f03ded5ec32b83fefd499a7166ab0bad0b1c25dd
Clay O'Neil <oneil512@umn.edu>
2024-06-20 23:44:05 -0500

    optimize

--- pages/1_Step 2: Draw Vectors.py
+++ pages/1_Step 2: Draw Vectors.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 from lib.aws import list_files_paginated, extract_first_frame, convert_lines_to_vectors, write_vectors_to_s3, convert_vectors_to_lines
 from lib.sagemaker_processing import run
-import importlib  
 import draw_lines
 from collections import defaultdict
 import logging
@@ -30,12 +29,6 @@ if 'vector_names' not in st.session_state:
 if 'names_to_vectors' not in st.session_state:
     st.session_state['names_to_vectors'] = defaultdict(list)
 
-refresh = st.button('Refresh Videos', key='refresh')
-
-if refresh:
-    names = list_files_paginated("jamar", "client_upload/", file_type='*')
-    st.session_state['vector_names'] = [name.split('/')[-1] for name in names]
-
 # Dropdown for selecting a background image
 bg_video_name = st.selectbox("Select a video to draw vectors on", st.session_state['vector_names'])
 
@@ -44,10 +37,9 @@ def get_first_frame(video_name):
     return extract_first_frame("jamar", f"client_upload/{video_name}")
 
 @st.cache_data
-def get_image_from_frame(frame):
-    return Image.fromarray(frame)
-
-lines = []
+def frame_to_base64(frame):
+    _, encoded_frame = cv2.imencode('.png', frame)
+    return base64.b64encode(encoded_frame).decode('utf-8')
 
 if bg_video_name:
     if 'bg_video_name' not in st.session_state or st.session_state['bg_video_name'] != bg_video_name:
@@ -56,48 +48,45 @@ if bg_video_name:
             image_height, image_width, _ = frame.shape
             st.session_state.image_height = image_height
             st.session_state.image_width = image_width
-            # convert array to bytes
-            _, encoded_frame = cv2.imencode('.png', frame)
-            bg_image = base64.b64encode(encoded_frame).decode('utf-8')
-            st.session_state['bg_image'] = bg_image
+            st.session_state['bg_image'] = frame_to_base64(frame)
             st.session_state['bg_video_name'] = bg_video_name
             st.session_state['canvas_result'] = None  # Clear canvas
 
+lines = []
 
 if 'bg_image' in st.session_state:
-    # print(f"Drawing lines, {st.session_state['names_to_vectors'][bg_video_name]}, {convert_vectors_to_lines(st.session_state['names_to_vectors'][bg_video_name])}")
-    # print(f"key is {st.session_state['bg_video_name']}_lines")
-    lines = draw_lines.draw_lines(st.session_state.bg_image, st.session_state.image_width, st.session_state.image_height, lines=convert_vectors_to_lines(st.session_state['names_to_vectors'][bg_video_name]), key=st.session_state['bg_video_name'] + "_lines")
-    # print(f"lines returned from component are {lines}")
+    lines = draw_lines.draw_lines(
+        st.session_state['bg_image'],
+        st.session_state['image_width'],
+        st.session_state['image_height'],
+        lines=convert_vectors_to_lines(st.session_state['names_to_vectors'][bg_video_name]),
+        key=st.session_state['bg_video_name'] + "_lines"
+    )
 
-
-if lines is not None and lines != []:
+if lines:
     vectors = convert_lines_to_vectors(lines)
     st.session_state['vectors'] = vectors
     st.session_state['names_to_vectors'][bg_video_name] = vectors
 
-
     for i, (x1, y1, x2, y2) in enumerate(vectors):
-        col1, col2 = st.columns(2)
-        # print(f"Vector {i + 1}")
-        
+        col1, col2 = st.columns([1, 2])
         with col1:
             st.write(f":blue[Vector {i + 1}]")
         with col2:
             directions_list = ["N", "E", "S", "W"]
-            option = None
             option = st.selectbox(f"Vector {i + 1} Direction", directions_list, key=f"direction_{i}")
             if option:
-                handle_click(option, i )
-    # Display the selected direction for each row
+                handle_click(option, i)
+
     if st.button("Save vectors and submit job"):
         file_type = st.session_state.get('bg_video_name').split('.')[-1]
 
         v = {}
-        # make a dictionary of directions to vectors
+        # Create a dictionary of directions to vectors
         for i, (x1, y1, x2, y2) in enumerate(vectors):
-            v[st.session_state.get(f'button_{i}')] = ((x1, y1), (x2, y2))
-        
+            v[st.session_state.get(f'button_{i}', 'Unknown')] = ((x1, y1), (x2, y2))
+
+        # Save vectors to S3
         write_vectors_to_s3(v, "jamar", f'submissions/{st.session_state.get("bg_video_name").replace("." + file_type, "")}/vectors.txt')
 
         # Run the processing job
@@ -105,11 +94,16 @@ if lines is not None and lines != []:
         st.write(f"Vectors saved!")
         st.write(f"Job submitted!")
 
-st.markdown("""
-**3. Check Status**: Click the following link to check the status of your submission.
-""")
-st.page_link(
-    "pages/1_Step 3: Traffic Tracker and Classifier.py",
-    label=":blue[Step 3: Traffic Tracker and Classifier]",
-    disabled=False
-)
+    refresh = st.button('Refresh Videos', key='refresh')
+    if refresh:
+        names = list_files_paginated("jamar", "client_upload/", file_type='*')
+        st.session_state['vector_names'] = [name.split('/')[-1] for name in names]
+
+    st.markdown("""
+    **3. Check Status**: Click the following link to check the status of your submission.
+    """)
+    st.page_link(
+        "pages/1_Step 3: Traffic Tracker and Classifier.py",
+        label=":blue[Step 3: Traffic Tracker and Classifier]",
+        disabled=False
+    )
